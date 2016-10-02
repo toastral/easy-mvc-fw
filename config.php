2cec322d32a8be8be63f98f67d2278ea3ee33937
toastral <toastral@gmail.com>
2016-10-02 19:13:23 +0400

    commit config

--- config.php
+++ config.php
@@ -5,4 +5,15 @@
  * Date: 18.09.2016
  * Time: 8:12
  */
-define('ROOT_PATH', '/vagrant/easy-mvc-fw/');
+define('ROOT_PATH', '/vagrant/easy-mvc-fw');
+
+define('VIEW_PATH', ROOT_PATH. '/app/views');
+
+
+define('ROOT_WWW', 'http://localhost:8080');
+
+
+
+
+
+
